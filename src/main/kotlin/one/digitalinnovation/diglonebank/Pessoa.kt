ce28ca6353600a7c7fc86990464879d9428d219a
alexpersilv8772 <89165026+alexpersilv8772@users.noreply.github.com>
2021-09-28 15:48:19 -0300

    updated to task aboutt class numbers
    Fala sobre a atualização dos membros da classe.

--- src/main/kotlin/one/digitalinnovation/diglonebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/diglonebank/Pessoa.kt
@@ -1,31 +1,41 @@
 package one.digitalinnovation.diglonebank
-//Classe pessoa criada dentro do pacote one.digitalinnovation.diglonebank
+//Primeira classe criada dentro do pacote one.digitalinnovation.diglonebank
 class Pessoa {
-    var name: String = "Alexandre"
-    var cpf: String = "123.123.123-11"
-    private set //Serve para que a variável cpf não seja alterada em outro lugar na aplicação. Ela atribui
-        // um valor fixo.
+    var name :  String  =  " Alexandre "
+    var cpf :  String  =  " 123.123.123-11 "
+    private set // Servir para que uma cpf variável não seja alterada em outro lugar na aplicação. Ela atribui
+    // um valor fixo.
 
-    //inner class Endereco { //inner class - classe interna ->
-        //var rua: String = "Rua Teste"
+    // classe interna Endereco {// classe interna - classe interna ->
+    // var rua: String = "Rua Teste"
 
-    //Obs: A classe interna pode ter modificadores de visibilidade: public , protected , private ou nenhum
-    // (privado ao pacote). A classe interna pode ser declarada com static caso ela não precise acessar nenhum
+    // Obs: A classe interna pode ter modificadores de visibilidade: público, protegido, privado ou nenhum
+    // (privado ao pacote). A classe interna pode ser declarada com caso estático ela não precisa acessar nenhum
     // membro da classe externa. Nesse caso, a classe interna pode ser instanciada mesmo sem uma instância da
-    // classe externa.    }
+    // classe externa. }
+
+    constructor() // Uma classe também pode declarar construtores secundários , que são prefixados com constructor
+
+    fun pessoaInf() = "O meu nome é $name e meu cof é $cpf." //recurso de interpolação ou intercalação de strings
+
+    // Uma classe em Kotlin pode ter um construtor primário e um ou mais construtores secundários . O construtor
+    // primário é uma parte do cabeçalho da classe e vai depois do nome da classe e dos parâmetros de tipo
+    // opcionais.
+
 
 }
 
 //Função principal para teste
 fun main () {
-    val suaIdentidade = Pessoa() // A variável suaIndetidade recebe a classe (instância) Pessoa com
-    // sua variáveis com a finalidade de criar um um objeto.
+    val suaIdentidade =  Pessoa () // A variável suaIndetidade recebe uma classe (instância) Pessoa com
+    // sua variáveis ​​com um fator de criar um objeto.
 
-    //suaIdentidade.cpf = "454" -> Serve para alterar a variável, caso ela não esteja em módulo privado.
-    //suaIdentidade.name = "Fernando" -> Serve para alterar a variável, caso ela não esteja em módulo privado.
+    // suaIdentidade.cpf = "454" -> Servir para alterar a variável, caso ela não esteja em módulo privado.
+    // suaIdentidade.name = "Fernando" -> Servir para alterar a variável, caso ela não está em módulo privado.
 
-    println(suaIdentidade.name)
-    println(suaIdentidade.cpf)
-    //println(suaIdentidade.Endereco().rua) -> Classe interna -> Inner Class
+    //println (suaIdentidade.name)
+    //println (suaIdentidade.cpf)
+    // println (suaIdentidade.Endereco (). rua) -> Classe interna -> Classe Interna
 
+    println(suaIdentidade.pessoaInf())
 }
