62a73f64055bed8de55ccbdbd43335fc1df9488f
alexpersilv8772 <89165026+alexpersilv8772@users.noreply.github.com>
2021-09-28 18:15:50 -0300

    Account classe creation - Error Correction
    Criação da conta classe - correção de erros.

--- src/main/kotlin/one/digitalinnovation/diglonebank/Conta.kt
+++ src/main/kotlin/one/digitalinnovation/diglonebank/Conta.kt
@@ -1,18 +1,18 @@
 package one.digitalinnovation.diglonebank
 
-import kotlin.math //rever a biblioteca importada
+//import kotlin.math  obs: rever a biblioteca importada
 
 class Conta {
     val agencia: String
     val numConta: String
     val saldo:  //Rever o tipo posteriormente.
-} {
-    fun depositos(valor:Float) {
+
+}
+    fun depositos(valor: Float) {
 
     }
 
-    fun saque(valor:Float){
-
+    fun saque(valor: Float) {
 
     }
 
