ded861f0fdab9cb61264f1f05bb02677bdc56097
alexpersilv8772 <89165026+alexpersilv8772@users.noreply.github.com>
2021-09-28 17:40:33 -0300

    account classe creation
    Criação da classe conta.

---
+++ src/main/kotlin/one/digitalinnovation/diglonebank/Conta.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovation.diglonebank
+
+import kotlin.math //rever a biblioteca importada
+
+class Conta {
+    val agencia: String
+    val numConta: String
+    val saldo:  //Rever o tipo posteriormente.
+} {
+    fun depositos(valor:Float) {
+
+    }
+
+    fun saque(valor:Float){
+
+
+    }
+
+}
